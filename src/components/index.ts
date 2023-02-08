e58c11fa7f6371c48f082ad7f56dee5987eb3499
Lea Hayes <lea.hayes@ext.ons.gov.uk>
2023-02-08 12:08:28 +0000

    Add missing `IconList` export from component listing.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./AffixList";
 export * from "./Heading";
 export * from "./Highlight";
+export * from "./IconList";
 export * from "./List";
 export * from "./Quote";
 export * from "./SectionScope";
