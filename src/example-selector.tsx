737a89b6ac86075e206ac68e65c98eff813769ec
Lea Hayes <lea.hayes@ext.ons.gov.uk>
2023-02-13 20:39:32 +0000

    Rename data attribute from `data-example-class` to `data-react-example-name`.

--- src/example-selector.tsx
+++ src/example-selector.tsx
@@ -2,8 +2,8 @@ import ReactDOM from "react-dom/client";
 
 import * as exampleComponents from "./example-components";
 
-for (let exampleElement of document.querySelectorAll("[data-example-class]")) {
-  const exampleComponentName = exampleElement.getAttribute("data-example-class");
+for (let exampleElement of document.querySelectorAll("[data-react-example-name]")) {
+  const exampleComponentName = exampleElement.getAttribute("data-react-example-name");
   const ExampleComponent = (exampleComponents as any)[exampleComponentName as string] as React.JSXElementConstructor<any>;
   ReactDOM.createRoot(exampleElement).render(<ExampleComponent/>);
 }
